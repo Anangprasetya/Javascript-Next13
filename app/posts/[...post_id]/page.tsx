1ddd24571929b0c793c20fa6f5a8ad2d6bae9277
Anang <tyoandini@gmail.com>
2023-10-15 13:17:01 +0700

    Install Tailwind

--- app/posts/[...post_id]/page.tsx
+++ app/posts/[...post_id]/page.tsx
@@ -1,7 +1,9 @@
 export default function PostDetail({params}: {params: {post_id: string}}) {
+  console.log(params);
+  
   return (
     <div>
-      PostDetail : {params.post_id}
+      PostDetail : {params.post_id[1]}
     </div>
   )
 }
