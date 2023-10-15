a0f345430a93cd5d32a7506a493618a81c094a10
Anang <tyoandini@gmail.com>
2023-10-15 14:17:15 +0700

    read data

---
+++ app/products/page.tsx
@@ -0,0 +1,50 @@
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+};
+
+async function getDataProduct() {
+    const res = await fetch("http://localhost:5000/products",{
+        cache: "no-store",
+      });
+    return res.json();
+}
+
+export default async function ProductList() {
+    const products: Product[] = await getDataProduct();
+
+    return (
+        <div className="py-10 px-10">
+            <div className="py-2">
+                Tambah Data
+            </div>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((data_product, index) => (
+                        <tr key={data_product.id}>
+                            <td>{index + 1}</td>
+                            <td>{data_product.title}</td>
+                            <td>{data_product.price}</td>
+                            <td className="flex">
+                                <div className="mr-1">
+                                    oke edit
+                                </div>
+
+                                oke delete
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
